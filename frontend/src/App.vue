13e581564bdb571da18bf2bc15fab57969df42a3
SiriusKoan <me@siriuskoan.one>
2022-09-23 17:32:33 +0800

    feat: setup vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import axios from "axios";
+import Day from "./components/Day.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Day,
+  },
+};
+</script>
+
+<template>
+  <main>
+  </main>
+</template>
