676495854a06b1c473a681d8065be445b6a21853
SiriusKoan <me@siriuskoan.one>
2023-01-28 04:26:43 -0500

    feat: add temp main page

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,32 +1,21 @@
 <template>
   <main>
-    <Day id="time-label" />
-    <Day id="Sun" weekday="Sun" />
-    <Day id="Mon" weekday="Mon" />
-    <Day id="Tue" weekday="Tue" />
-    <Day id="Wed" weekday="Wed" />
-    <Day id="Thu" weekday="Thu" />
-    <Day id="Fri" weekday="Fri" />
-    <Day id="Sat" weekday="Sat" />
+      <h1>Week</h1>
   </main>
 </template>
 
 <script>
-import Day from "../components/Day.vue";
 export default {
   name: "MainPage",
-  components: {
-    Day,
-  },
 };
 </script>
 
 <style scoped>
 main {
-  position: absolute;
-  margin: auto 0;
-  width: 100%;
-  display: flex;
-  justify-content: center;
+    height: 90vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 50px;
 }
 </style>
