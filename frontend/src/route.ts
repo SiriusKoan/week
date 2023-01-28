d2d013e2de206fc4d73b4883a5b1e469003bd9e4
SiriusKoan <me@siriuskoan.one>
2023-01-28 02:16:05 -0500

    feat: add timetable page

--- frontend/src/route.ts
+++ frontend/src/route.ts
@@ -5,6 +5,8 @@ import LoginPage from "./views/LoginPage.vue";
 import LogoutPage from "./views/LogoutPage.vue";
 import RegisterPage from "./views/RegisterPage.vue";
 import AboutPage from "./views/AboutPage.vue";
+import TimetablePage from "./views/TimetablePage.vue";
+import CreateTimetablePage from "./views/CreateTimetablePage.vue";
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -14,5 +16,7 @@ export const router = createRouter({
     { path: "/logout", component: LogoutPage },
     { path: "/register", component: RegisterPage },
     { path: "/about", component: AboutPage },
+    { path: "/create", component: CreateTimetablePage },
+    { path: "/:email/:name", component: TimetablePage, props: true },
   ],
 });
---
+++ frontend/src/views/TimetablePage.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <Day id="time-label" />
+    <Day
+      id="Sun"
+      weekday="Sun"
+      :schedule="[
+        [480, 720, 'test 1'],
+        [1000, 1200, 'test 2'],
+      ]"
+    />
+    <Day id="Mon" weekday="Mon" :schedule="[[360, 420, 'event a']]" />
+    <Day id="Tue" weekday="Tue" />
+    <Day id="Wed" weekday="Wed" />
+    <Day id="Thu" weekday="Thu" />
+    <Day id="Fri" weekday="Fri" />
+    <Day id="Sat" weekday="Sat" />
+  </main>
+</template>
+
+<script>
+import Day from "../components/Day.vue";
+export default {
+  name: "TimetablePage",
+  components: {
+    Day,
+  },
+  props: {
+      email: String,
+      name: String,
+  },
+  created() {
+      console.log(this.email, this.name);
+  }
+};
+</script>
+
+<style scoped>
+main {
+  position: absolute;
+  margin: auto 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+</style>
