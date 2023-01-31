5a461b5d4d255dc40d6047cd6edf94921839fa04
SiriusKoan <me@siriuskoan.one>
2023-01-31 01:20:59 -0500

    feat: connect create timetable page to backend

--- frontend/src/views/CreateTimetablePage.vue
+++ frontend/src/views/CreateTimetablePage.vue
@@ -1,57 +1,83 @@
 <template>
   <main>
-      <input type="text" v-model="name" placeholder="Timetable Name" required>
-      <CreateTimetableForm @add="addTimeCell" />
-      <button @click="sendCreate">Create</button>
-      <div id="timetable-container">
-          <Day id="time-label" />
-          <Day id="Sun" weekday="Sun" />
-          <Day id="Mon" weekday="Mon" />
-          <Day id="Tue" weekday="Tue" />
-          <Day id="Wed" weekday="Wed" />
-          <Day id="Thu" weekday="Thu" />
-          <Day id="Fri" weekday="Fri" />
-          <Day id="Sat" weekday="Sat" />
-      </div>
+    <input type="text" v-model="name" placeholder="Timetable Name" required />
+    <CreateTimetableForm @add="addTimeCell" />
+    <button @click="sendCreate">Create</button>
+    <div id="timetable-container">
+      <Day id="time-label" />
+      <Day id="Sun" weekday="Sun" :schedule="schedule[0]" />
+      <Day id="Mon" weekday="Mon" :schedule="schedule[1]" />
+      <Day id="Tue" weekday="Tue" :schedule="schedule[2]" />
+      <Day id="Wed" weekday="Wed" :schedule="schedule[3]" />
+      <Day id="Thu" weekday="Thu" :schedule="schedule[4]" />
+      <Day id="Fri" weekday="Fri" :schedule="schedule[5]" />
+      <Day id="Sat" weekday="Sat" :schedule="schedule[6]" />
+    </div>
   </main>
 </template>
 
 <script>
 import Day from "../components/Day.vue";
 import CreateTimetableForm from "../components/CreateTimetableForm.vue";
+import { toRaw } from "vue";
+import axios from "axios";
+import { getAuth } from "firebase/auth";
 export default {
-    name: "CreateTimetablePage",
-    components: {
-        Day,
-        CreateTimetableForm,
+  name: "CreateTimetablePage",
+  components: {
+    Day,
+    CreateTimetableForm,
+  },
+  data() {
+    return {
+      name: "",
+      timetable: [],
+      schedule: [...Array(7)].map((x) => []),
+    };
+  },
+  methods: {
+    addTimeCell(name, location, weekday, start_time, end_time) {
+      start_time = start_time.split(":");
+      let start_time_num =
+        parseInt(start_time[0]) * 60 + parseInt(start_time[1]);
+      end_time = end_time.split(":");
+      let end_time_num = parseInt(end_time[0]) * 60 + parseInt(end_time[1]);
+      this.timetable.push({
+        name: name,
+        location: location,
+        day: weekday,
+        start_time: start_time_num,
+        end_time: end_time_num,
+      });
+      this.schedule[weekday].push([
+        start_time_num,
+        end_time_num,
+        name,
+        location,
+      ]);
     },
-    data() {
-        return {
-            name: "",
-            timetable: [],
-        }
+    sendCreate() {
+      const auth = getAuth();
+      let token = "";
+      let data = { name: this.name, timetable: toRaw(this.timetable) };
+      auth.onAuthStateChanged((user) => {
+        token = `${user.accessToken}`;
+        let headers = { Authorization: `Bearer ${token}` };
+        axios.post("/api/create", data, { headers: headers }).then((res) => {
+          console.log(res);
+        });
+      });
     },
-    methods: {
-        addTimeCell(name, location, weekday, start_time, end_time) {
-            start_time = start_time.split(":");
-            let start_time_num = parseInt(start_time[0]) * 60 + parseInt(start_time[1]);
-            end_time = end_time.split(":");
-            let end_time_num = parseInt(end_time[0]) * 60 + parseInt(end_time[1]);
-            this.timetable.push({"name": name, "location": location, "weekday": weekday, "start_time": start_time_num, "end_time": end_time_num});
-        },
-        sendCreate() {
-            console.log(this.name, this.timetable);
-        }
-    },
+  },
 };
 </script>
 
 <style scoped>
 #timetable-container {
-    position: absolute;
-    margin: auto 0;
-    width: 100%;
-    display: flex;
-    justify-content: center;
+  position: absolute;
+  margin: auto 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
 }
 </style>
