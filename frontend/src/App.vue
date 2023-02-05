3f1aa35f6642a70ceea43231605e831d67bfe40a
SiriusKoan <me@siriuskoan.one>
2023-02-04 22:23:33 -0500

    feat: add notification

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,38 @@
 <script lang="ts">
 import NavBar from "./components/NavBar.vue";
 import CopyrightFooter from "./components/CopyrightFooter.vue";
+import FloatingNotification from "./components/FloatingNotification.vue";
 export default {
   data() {
-    return {};
+    return {
+      notify_status: "",
+      notify_msg: "",
+    };
   },
   components: {
     NavBar,
     CopyrightFooter,
+    FloatingNotification,
+  },
+  methods: {
+    notify(notify_status, notify_msg) {
+      this.notify_status = notify_status;
+      this.notify_msg = notify_msg;
+    },
+  },
+  watch: {
+    $route(to, from) {
+      this.notify_status = "";
+      this.notify_msg = "";
+    },
   },
 };
 </script>
 
 <template>
   <NavBar />
-  <router-view />
+  <FloatingNotification :status="notify_status" :msg="notify_msg" />
+  <router-view @notify="notify" />
   <CopyrightFooter />
 </template>
 
---
+++ frontend/src/components/FloatingNotification.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container" v-if="status">
+    <div :class="status" class="notification">
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FloatingNotification",
+  props: {
+    status: String,
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  width: 100%;
+  z-index: 100;
+  margin: 0;
+}
+
+.notification {
+  color: var(--text-color);
+  -webkit-box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.5);
+  -moz-box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.5);
+  box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.5);
+  width: 50vw;
+  padding: 10px;
+  margin: 3px auto;
+  text-align: center;
+}
+
+.error {
+  background-color: #f44336;
+}
+
+.warning {
+  background-color: #ffeb3b;
+}
+
+.success {
+  background-color: #4caf50;
+}
+</style>
