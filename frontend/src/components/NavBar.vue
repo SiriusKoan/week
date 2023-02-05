8312a73bce248862c5e01f6526caedd19f0f9dbb
SiriusKoan <me@siriuskoan.one>
2023-02-04 23:38:15 -0500

    feat: move logout to navbar button

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -11,30 +11,36 @@
     <router-link v-if="authed" to="/create" active-class="activate">
       Create
     </router-link>
-    <router-link v-if="authed" to="/logout" active-class="activate">
-      Logout
-    </router-link>
+    <div v-if="authed" id="logout-btn" @click="logout">Logout</div>
   </nav>
 </template>
 
 <script>
 import { getAuth } from "firebase/auth";
+import { useRouter } from "vue-router";
 export default {
   name: "NavBar",
   data() {
     return {
       authed: false,
+      auth: getAuth(),
+      router: useRouter(),
     };
   },
   created() {
-    const auth = getAuth();
-    auth.onAuthStateChanged((user) => {
+    this.auth.onAuthStateChanged((user) => {
       if (user) {
         this.authed = true;
       } else {
         this.authed = false;
       }
     });
+  },
+  methods: {
+      logout() {
+          this.auth.signOut();
+          this.router.push("/");
+      },
   },
 };
 </script>
@@ -52,7 +58,20 @@ nav a {
   text-decoration: none;
 }
 
+nav a:hover {
+    text-decoration: underline;
+}
+
 .activate {
   color: #cf6e6e;
+}
+
+#logout-btn {
+    color: #cccc6e;
+    cursor: pointer;
+}
+
+#logout-btn:hover {
+    text-decoration: underline;
 }
 </style>
